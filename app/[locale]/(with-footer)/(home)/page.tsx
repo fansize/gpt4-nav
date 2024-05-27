f8a97b10a4ab2b98f4128d4620fd7df0b08ebc71
TangLang <langlang_1991@126.com>
2024-05-27 16:11:49 +0300

    增加一个分类标签按钮

--- app/[locale]/(with-footer)/(home)/page.tsx
+++ app/[locale]/(with-footer)/(home)/page.tsx
@@ -6,6 +6,7 @@ import { CircleChevronRight } from 'lucide-react';
 import { getTranslations } from 'next-intl/server';
 
 import { WebNavigationListRow } from '@/lib/data';
+import { Button } from '@/components/ui/button';
 import WebNavCardList from '@/components/webNav/WebNavCardList';
 
 const ScrollToTop = dynamic(() => import('@/components/page/ScrollToTop'), { ssr: false });
@@ -27,7 +28,7 @@ export async function generateMetadata({ params: { locale } }: { params: { local
   };
 }
 
-export const revalidate = 3600;
+const featureList: string[] = ['Most popular', 'Hot', 'New', 'Recommended', 'Recently updated'];
 
 export default async function Page() {
   const t = await getTranslations('Home');
@@ -47,32 +48,46 @@ export default async function Page() {
   );
 
   return (
-    <>
-      <section id='products gallery' className='container py-12 sm:py-24'>
-        <div className='text-center'>
-          <h1 className='text-3xl font-bold md:text-4xl '>{t('title')}</h1>
-          <h2 className='mx-auto mb-8 mt-4 text-xl text-muted-foreground md:w-3/4'>{t('subTitle')}</h2>
-        </div>
+    <section id='products gallery' className='container py-12 sm:py-24'>
+      {/* 标题 */}
+      <div className='text-center'>
+        <h1 className='text-3xl font-bold md:text-4xl '>{t('title')}</h1>
+        <h2 className='mx-auto mb-8 mt-4 text-xl text-muted-foreground md:w-3/4'>{t('subTitle')}</h2>
+      </div>
 
-        <div className='flex flex-col gap-6'>
-          {Object.keys(groupedData).map((categoryName) => (
-            <div key={categoryName}>
-              <h2 className='mb-2 text-left text-[18px] lg:text-[32px]'>{categoryName}</h2>
-              <WebNavCardList key={categoryName} dataList={groupedData[categoryName]} />
-            </div>
-          ))}
+      {/* 分类标签 */}
+      <div className='md:justify-left my-8 flex flex-wrap gap-4'>
+        {featureList.map((feature: string) => (
+          <div key={feature}>
+            <Button variant='outline' size='sm' className='text-xs'>
+              {feature}
+            </Button>
+          </div>
+        ))}
+      </div>
 
-          <Link
-            href='/explore'
-            className='mx-auto my-8 flex w-fit items-center justify-center gap-5 rounded-[9px] border p-[10px] text-sm leading-4 hover:opacity-70'
-          >
-            {t('exploreMore')}
-            <CircleChevronRight className='mt-[0.5] h-[20px] w-[20px]' />
-          </Link>
-        </div>
-      </section>
+      {/* 产品列表 */}
+      <div className='flex flex-col gap-6'>
+        {Object.keys(groupedData).map((categoryName) => (
+          <div key={categoryName}>
+            <h2 className='mb-3 bg-gradient-to-b from-primary/60 to-primary bg-clip-text text-3xl font-bold text-transparent md:text-4xl'>
+              {categoryName}
+            </h2>
+            <WebNavCardList key={categoryName} dataList={groupedData[categoryName]} />
+          </div>
+        ))}
+      </div>
+
+      {/* 更多标签 */}
+      <Link
+        href='/explore'
+        className='mx-auto my-8 flex w-fit items-center justify-center gap-5 rounded-[9px] border p-[10px] text-sm leading-4 hover:opacity-70'
+      >
+        {t('exploreMore')}
+        <CircleChevronRight className='mt-[0.5] h-[20px] w-[20px]' />
+      </Link>
 
       <ScrollToTop />
-    </>
+    </section>
   );
 }
