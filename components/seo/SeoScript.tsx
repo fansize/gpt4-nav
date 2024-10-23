f03923a11941eca7a8fbf29d1025dc4be7dbadf0
fansize <langlang_1991@126.com>
2024-10-23 18:17:44 +0200

    v2.1.3 增加umami

--- components/seo/SeoScript.tsx
+++ components/seo/SeoScript.tsx
@@ -5,6 +5,11 @@ import { GOOGLE_TRACKING_ID } from '@/lib/env';
 export default function SeoScript() {
   return (
     <>
+      <Script
+        src="https://cloud.umami.is/script.js"
+        data-website-id="262dc8a3-e37b-4b0f-86c2-cddbdcfd18da"
+        strategy="afterInteractive"
+      />
       <Script strategy='afterInteractive' src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_TRACKING_ID}`} />
       <Script
         id='gtag-init'
